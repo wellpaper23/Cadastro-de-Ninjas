d924251c473143cb52f6bb95499b277c32988db1
Well <wevelle98@gmail.com>
2025-05-04 18:29:31 -0300

    fix: change NinjaRepository class to an interface

--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaRepository.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaRepository.java
@@ -1,5 +1,6 @@
 package dev.java10x.cadastrodeninjas.Ninjas;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class NinjaRepository {
+public interface NinjaRepository extends JpaRepository<NinjaModel,Long> {
 
 }
