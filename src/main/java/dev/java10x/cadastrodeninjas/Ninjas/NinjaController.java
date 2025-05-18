8cf8106469c2a31d323a88fad6450d5db3b06b2f
Well <wevelle98@gmail.com>
2025-05-18 11:37:18 -0300

    feat: add mock data handling to NinjaControler

--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.java10x.cadastrodeninjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -10,6 +8,32 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String BoasVindas(){
         return "Boas vindas, essa é a minha primeira mensagem nessa rota";
+    }
+
+    //adicionar ninja
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja criado com sucesso";
+    }
+
+    @GetMapping("/mostrarID")
+    public String mostrarNinjaPorId(){
+        return "Ninja encontrado";
+    }
+
+    @GetMapping("/todosID")
+    public String mostrarNinja(){
+        return "Mostrando todos os Ninjas com ID";
+    }
+
+    @PutMapping("/editarID")
+    public String editarNinja(){
+        return "Ninja editado com sucesso";
+    }
+
+    @DeleteMapping("/deletarID")
+    public String deletarNinja(){
+        return "Ninja deletado com sucesso";
     }
 
 }
